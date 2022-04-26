4e16821b7e45f2f9ececc2d0a0d7d3dd1f39370b
shogoBCN <56768230+shogoBCN@users.noreply.github.com>
2022-04-26 07:45:05 +0200

    logic done

--- js/cart.js
+++ js/cart.js
@@ -1,18 +1,9 @@
-
-
-
 timeout = 0;
 
-
-
-
-
-
 let itemTotalPrices = function (ele) {
-  console.log(ele);
   let prices = parseFloat($(ele).find('.single-price div').text()); 
   let quants = parseFloat($(ele).find('.quantity input').val()); 
-  let total = (prices * quants).toFixed(2); 
+  let total = parseFloat((prices * quants).toFixed(2)); 
   $(ele).children('.total-price').html(total + "€");
   return total;
 };
@@ -27,14 +18,42 @@ $(document).on('input', 'tr input', function () {
 let fireAll = function () {
   let pricesArr = [];
   finalPrice = 0;
-  $('tbody tr').not('.final-price').each(function (i, ele) { //how do I exclude #final-price
+  $('.items').each(function (i, ele) {  
     pricesArr.push(itemTotalPrices(ele));
    });
-  console.log(pricesArr)
-  finalPrice = pricesArr.reduce((a, b) => a + b);
-  $('.final-price').html(finalPrice);
+
+  if (pricesArr.length == 0) {
+    $('.final-price').html("-");
+  }
+  else {
+    finalPrice = pricesArr.reduce((a, b) => a + b).toFixed(2);
+    $('.final-price').html(finalPrice + "€");
+  }
 }
 
 $(document).ready(function () {
-  fireAll()
+  fireAll();
+  $(document).on('click', '.btn.remove', function () {
+    $(this).closest('tr').remove();
+    fireAll()
+  });
+
+  $('#add-item').on('submit', function (event) {
+    event.preventDefault();
+    var quant = 1;
+    var name = $(this).children('#item-name').val();
+    var price = $(this).children('#item-price').val();
+
+    $('tbody:last').prepend('<tr class="items">' +
+      '<td class="name">' + name + '</td>' +
+      '<td class="single-price"><div>' + price + '</div></td>' +
+      '<td class="quantity"><input type="number" value="' + quant + '" /></td>' +
+      '<td><button class="btn btn-light btn-sm remove">remove</button></td>' +
+      '<td class="total-price"></td>' +
+    '</tr>');
+
+    fireAll()
+    $(this).children('#item-name').val('');
+    $(this).children('#item-price').val('');
+  });
 });
